8b51d97bf189f00b648a16c102db0803e8ab6620
Joeva-code <aduguj@gmail.com>
2026-05-22 17:12:04 +0100

    finalfinal commit

--- Src/config/database.js
+++ Src/config/database.js
@@ -2,14 +2,9 @@ const { Pool } = require('pg');
 const logger = require('../utils/logger.js');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  max: 20, // maximum number of clients in the pool
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: 
+    process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 // Test database connection
